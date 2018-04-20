075ab87075bc5f5671943ce84f5e6c210d50f80c
Swapan kumar Das <sdbit04@gmail.com>
2018-04-20 14:35:36 +0530

    Update firefox spelling

--- venv/Include/MySitePOM/TestPkg/LoginPageTest.py
+++ venv/Include/MySitePOM/TestPkg/LoginPageTest.py
@@ -1,7 +1,7 @@
 from Include.PytestFrameWorkApproch.BasePkg.DriverClassModule import DriverCreator
 from Include.PytestFrameWorkApproch.PagePkg.LoginPage import LoginPage
 import unittest
-Browser="Firefox"
+Browser="firefox"
 # Try to use fixture at here
 LoginPageOB=LoginPage(Browser)
 class LoginTestClass(unittest.TestCase):
