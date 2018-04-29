da14c6c675442b668d90168fa5d516d043fcbf5a
Swapan kumar Das <sdbit04@gmail.com>
2018-04-30 00:12:41 +0530

    refactor the name of test_module

--- venv/Include/MySitePOM/TestPkg/test_LoginPage.py
+++ venv/Include/MySitePOM/TestPkg/test_LoginPage.py
@@ -3,9 +3,15 @@ from Include.MySitePOM.PagePkg.LoginPage import LoginPage
 import unittest
 import pytest
 Browser="FirefoX"
+@pytest.fixture()
+def testSetup():
+    LoginPageOB = LoginPage(Browser)
+    print("Setting up data")
+    return LoginPageOB
+
 # Try to use fixture at here
-LoginPageOB=LoginPage(Browser)
-class LoginTestClass(unittest.TestCase):
+# LoginPageOB=LoginPage(Browser)
+class TestLoginClass():
     #We have inheritade the unittest.TestCase class to allow all the methodes into class prefix test_ will get executed by py.test
     #If we dont do this, then py.test will report that, there is no test cases to be executed
     # def test_login(self):
@@ -13,8 +19,9 @@ class LoginTestClass(unittest.TestCase):
     #     LoginPageob = LoginPage(Driver)
     #     LoginPageob.clickToLoginLink()
 
-    def test_invalidLogin(self):
-        LoginPageOB.invalidLogin('sdbit04@gmail.com', 'abcd')
-        # Element, TorF=LoginPageOB.getElementWithResult('xpath', "//div[contains(text(), 'Invalid email or password')]")
-        Element, TorF=LoginPageOB.getElementWithResult("xpath", "//div[contains(text(), 'Invalid email or password')]")
+    def test_invalidLogin(self, testSetup):
+        # LoginPageOB.invalidLogin('sdbit04@gmail.com', 'abcd')
+        testSetup.invalidLogin('sdbit04@gmail.com', 'abcd')
+        # Element, TorF=LoginPageOB.getElementWithResult("xpath", "//div[contains(text(), 'Invalid email or password')]")
+        Element, TorF=testSetup.getElementWithResult("xpath", "//div[contains(text(), 'Invalid email or password')]")
         assert TorF == True
